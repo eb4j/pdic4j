cd801508c9396d0d8936aaee22d2f993a12c5a4c
Hiroshi Miura <miurahr@linux.com>
2022-01-09 13:18:54 +0900

    Add more tests
    - With attribution
    - RTL language
    - With cache creation
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/test/kotlin/io/github/eb4j/pdic/PdicDictionaryTest.kt
+++ src/test/kotlin/io/github/eb4j/pdic/PdicDictionaryTest.kt
@@ -22,6 +22,8 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.function.Executable
 import java.io.File
+import java.nio.file.Path
+import kotlin.io.path.deleteExisting
 
 class PdicDictionaryTest {
 
@@ -43,6 +45,56 @@ class PdicDictionaryTest {
                 )
                 return
             }
+    }
+
+    @Test
+    fun getEntriesWithRTL() {
+        PdicDictionary.loadDictionary(file, cache)
+            .getEntries("persian").forEach {
+                assertAll(
+                    Executable { assertEquals("سلام علیکم", it.translation) },
+                    Executable { assertEquals("persian", it.indexWord) },
+                    Executable { assertEquals("Persian", it.headWord) },
+                    Executable { assertEquals(0x00, it.attribute) },
+                    Executable { assertNull(it.pronunciation) },
+                    Executable { assertNull(it.example) }
+                )
+                return
+            }
+    }
+
+    @Test
+    fun getEntriesWithAttribute() {
+        PdicDictionary.loadDictionary(file, cache)
+            .getEntries("vietnamese").forEach {
+                assertAll(
+                    Executable { assertEquals("Chào anh,Chào chi", it.translation) },
+                    Executable { assertEquals("vietnamese", it.indexWord) },
+                    Executable { assertEquals("Vietnamese", it.headWord) },
+                    Executable { assertEquals(0x10, it.attribute) },
+                    Executable { assertNull(it.pronunciation) },
+                    Executable { assertNull(it.example) }
+                )
+                return
+            }
+    }
+
+    @Test
+    fun getEntriesWithCacheCreation() {
+        val newCache: Path = kotlin.io.path.createTempFile("pdic4j")
+        PdicDictionary.loadDictionary(file, newCache.toFile())
+            .getEntries("japanese").forEach {
+                assertAll(
+                    Executable { assertEquals("こんにちは", it.translation) },
+                    Executable { assertEquals("japanese", it.indexWord) },
+                    Executable { assertEquals("Japanese", it.headWord) },
+                    Executable { assertEquals(0, it.attribute) },
+                    Executable { assertNull(it.pronunciation) },
+                    Executable { assertNull(it.example) }
+                )
+                return
+            }
+        newCache.deleteExisting()
     }
 
     @Test
